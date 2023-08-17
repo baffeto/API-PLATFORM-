3163216fc14a589c913422f0304104dc74bf0c18
baffeto <baffeto@mail.ru>
2023-08-17 13:10:57 +0300

    Create my custom router | decorator action | SimpleRouter vs DefaultRouter | queryset changed

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,9 +2,33 @@ from .views import ShopViewSet, ProductViewSet
 from django.urls import path, include
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'shop', ShopViewSet)
+# SimpleRouter - нет представления всех маршрутов /api/v1/
+# DefaultRouter - имеет представление всех маршрутов /api/v1/shop | /api/v1/product
+
+# class MyCustomRouter(routers.DefaultRouter):
+#     routes = [
+#         routers.Route(
+#             url=r'^{prefix}$',
+#             mapping={'get': 'list'},
+#             name='{basename}-list',
+#             detail=False,
+#             initkwargs={'suffix': 'List'}
+#         ),
+#         routers.Route(
+#             url=r'^{prefix}/{lookup}$',
+#             mapping={'get': 'retrieve'},
+#             name='{basename}-detail',
+#             detail=True,
+#             initkwargs={'suffix': 'Detail'}
+#         )
+#     ]
+
+# router = MyCustomRouter()
+router = routers.DefaultRouter()
+router.register(r'shop', ShopViewSet, basename='shop')
 router.register(r'product', ProductViewSet)
+
+print(router.urls)
 
 app_name = 'api'
 
--- project/api/views.py
+++ project/api/views.py
@@ -2,6 +2,7 @@ from .models import Shop, Product
 from .serializers import ShopSerializer, ProductSerializer
 from rest_framework.views import APIView
 from rest_framework import generics, viewsets, mixins
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -15,15 +16,31 @@ class ShopViewSet(mixins.CreateModelMixin,
                   mixins.ListModelMixin,
                   GenericViewSet):
     
-    queryset = Shop.objects.all()
+    # queryset = Shop.objects.all()
     serializer_class = ShopSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        
+        if not pk:
+            return Shop.objects.all()[:6]
+        
+        return Shop.objects.filter(pk=pk)
+    
+    @action(methods=['get'], detail=True)
+    def product(self, request, pk=None):
+        # http://127.0.0.1:8000/api/v1/shop/4/product/
+        product = Product.objects.get(pk=pk)
+        return Response({
+            'product': product.name
+        })
 
 class ProductViewSet(mixins.CreateModelMixin,
-                    mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    mixins.ListModelMixin,
-                    GenericViewSet):
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     GenericViewSet):
     
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
